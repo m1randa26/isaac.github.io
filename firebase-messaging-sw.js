ec5c9443432e9845395f3e803bb20e97608523e4
rxlxndx <20223tn027@utez.edu.mx>
2025-11-24 12:19:29 -0600

    rutas correctas 2

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -19,7 +19,7 @@ messaging.onBackgroundMessage((payload) => {
   const title = payload.notification?.title || "Notificación";
   const options = {
     body: payload.notification?.body || "",
-    icon: "/icon-192.png"
+    icon: "./icons-192.png"
   };
   self.registration.showNotification(title, options);
 });
